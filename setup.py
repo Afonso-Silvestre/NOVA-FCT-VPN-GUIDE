ee7a686146f9fa91f82ed6b8edf9604c18080d87
Afonso-Silvestre <asilvestrecontact@example.com>
2026-06-20 19:55:03 +0100

    Naming of funcs

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import shutil, os, stat
 from pathlib import Path
-from utils import moveToDesktop, makeExec, writeToFile
+from utils import move_to_desktop, make_executable, write_to_file
 
 BASH_PATH = Path("vpn.sh")
 BASH_SCRIPT: str = "#!/bin/bash\necho \"RUNNING NOVA FCT VPN\"\nsudo snx-rs -s vpn.fct.unl.pt -u CLIP_USERNAME -o vpn_Username_Password -X true -T tcpt"
@@ -10,10 +10,10 @@ SHORTCUT_SCRIPT: str = "[Desktop Entry]\nType=Application\nName=VPN\nExec=PATH\n
 
 username = input("What is your CLIP username?\n")
 
-writeToFile(BASH_PATH, BASH_SCRIPT.replace("CLIP_USERNAME", username, 1))
-writeToFile(SHORTCUT_PATH, SHORTCUT_SCRIPT.replace("PATH", str(BASH_PATH.resolve()), 1))
+write_to_file(BASH_PATH, BASH_SCRIPT.replace("CLIP_USERNAME", username, 1))
+write_to_file(SHORTCUT_PATH, SHORTCUT_SCRIPT.replace("PATH", str(BASH_PATH.resolve()), 1))
 
-makeExec(BASH_PATH)
-makeExec(SHORTCUT_PATH)
+make_executable(BASH_PATH)
+make_executable(SHORTCUT_PATH)
 
-moveToDesktop(SHORTCUT_PATH)
+move_to_desktop(SHORTCUT_PATH)
--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import shutil, os, stat
 
-def writeToFile(f: Path, text: str):
+def write_to_file(f: Path, text: str):
     with open(f, "w", encoding="utf-8") as file:
         file.write(text)
 
-def makeExec(f: Path):
+def make_executable(f: Path):
     os.chmod(f, os.stat(f).st_mode | stat.S_IEXEC)
 
-def moveToDesktop(src: Path) -> None:
+def move_to_desktop(src: Path) -> None:
     shutil.move(src, Path.home() / "Desktop" / src.name)
